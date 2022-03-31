7b80cc0ee6e358b32cff10421c30a14e3cf1b1d3
azat valiev <azvaliev@gmail.com>
2022-03-31 15:32:24 -0700

    Base homepage functional

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,21 @@ const Home: NextPage = () => {
   return (
     <main className={classes.main}>
       <section className={classes.productHighlight}>
-        {/*
+        
         <div className={classes.introText}>
-          <h1>The latest in Men&apos;s Fashion</h1>
-        </div> */}
+          <h1>
+            The Distinguished Man
+          </h1>
+          <span>
+            With dozens of options to choose from,<br/>
+            there&apos;s a perfect suit for everybody
+          </span>
+          <div className={classes.buttonWrapper}>
+            <button className="btn btn-outline">
+              View Latest Styles
+            </button>
+          </div>
+        </div>
         <div className={classes.productWrapper}>
           <Image 
             src="/img/models/man-in-suit-crossing-street.jpg"
