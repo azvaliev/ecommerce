1386ed56b49d2fe880a2c50d4a3a9101d241fae7
azat valiev <azvaliev@gmail.com>
2022-04-01 13:17:34 -0700

    Functional product filtering

--- components/products/ProductFilters.tsx
+++ components/products/ProductFilters.tsx
@@ -1,59 +1,86 @@
+import { ChangeEvent, useContext } from "react";
+import { filterContext } from "../../pages/products/all";
 import classes from "../../styles/modules/ProductFilter.module.scss";
 
-const filterOpts: Array<Array<FilterObj>> = [
-	[
-		{
-			value: "",
-			displayValue: "Product Type"			
-		},
-		{
-			value: "shirt",
-			displayValue: "Shirts"
-		},
-		{
-			value: "vest",
-			displayValue: "Suit Vests"
-		},
-		{
-			value: "jacket",
-			displayValue: "Suit Jackets"
-		}
-	], [
-		{
-			value: "",
-			displayValue: "Style"
-		},
-		{
-			value: "plain",
-			displayValue: "Plain Color"
-		},
-		{
-			value: "plaid",
-			displayValue: "Plaid"
-		}
-	], [
-		{
-			value: "",
-			displayValue: "Standard Edition"
-		},
-		{
-			value: "limited",
-			displayValue: "Limited Edition"
-		}
-	]		
+const allFilterOpts: filterSettings[] = [
+	{
+		type: "productType",
+		options: [
+			{
+				value: "",
+				displayValue: "All Products"			
+			},
+			{
+				value: "shirt",
+				displayValue: "Shirts"
+			},
+			{
+				value: "vest",
+				displayValue: "Suit Vests"
+			},
+			{
+				value: "jacket",
+				displayValue: "Suit Jackets"
+			}
+		]
+	},  {
+		type: "style",
+		options:[
+			{
+				value: "",
+				displayValue: "Any Style"
+			},
+			{
+				value: "plain",
+				displayValue: "Plain Color"
+			},
+			{
+				value: "striped",
+				displayValue: "Striped"
+			},
+			{
+				value: "plaid",
+				displayValue: "Plaid"
+			}
+		]
+	}, {
+		type: "edition",
+		options: [
+			{
+				value: "",
+				displayValue: "Standard Edition"
+			},
+			{
+				value: "limited",
+				displayValue: "Limited Edition"
+			}
+		]	
+	}	
 ]
-
-interface FilterObj {
+interface filterSettings {
+	type: string;
+	options: filterObj[]
+}
+interface filterObj {
 	value: string;
 	displayValue: string;
 }
-interface FilterProps {
-	options: FilterObj[];
+interface filterProps {
+	allFilterOpts: filterSettings[];
 }
 
-const Filter = ({options}: FilterProps) => {
+const Filter = ({type, options}: filterSettings) => {
+
+	const {filterState, updateFilterState} = useContext(filterContext);
+
+	const handleUpdateFilterState = (e: ChangeEvent<HTMLSelectElement>) => {
+		updateFilterState!({
+			[type]: e.target.value
+		})
+	}
+
 	return (
-		<select className={classes["filter"]}>
+		<select className={classes["filter"]} onChange={handleUpdateFilterState}>
 			{options.map((setting, idx) => 
 				<option value={setting.value} key={idx}>
 					{setting.displayValue}
@@ -68,8 +95,8 @@ const ProductFilters = () => {
 	return (
 		<div className={classes["wrapper-filters"]}>
 			<h4>Filters: </h4>
-			{filterOpts.map((opt, idx) =>
-				<Filter options={opt} key={idx} />
+			{allFilterOpts.map((filter, idx) =>
+				<Filter type={filter.type} options={filter.options} key={idx} />
 			)}
 		</div>
 	)
--- pages/products/all.tsx
+++ pages/products/all.tsx
@@ -1,26 +1,75 @@
 import { GetStaticProps } from "next";
-import { ProductArray } from "../../components/products/ProductTypes";
+import { Product, ProductArray } from "../../components/products/ProductTypes";
 import classes from "../../styles/modules/Products.module.scss";
 
 import ProductPreview from "../../components/products/ProductPreview";
 import ProductFilters from "../../components/products/ProductFilters";
+import { createContext, useReducer } from "react";
+
+// Types
+
+interface filterOpts {
+	productType?: string;
+	style?: string;
+	edition?: string;
+}
+
+const initialFilter: filterOpts = {
+	productType: "",
+	style: "",
+	edition: ""
+}
+
+interface filterContext {
+	filterState?: filterOpts;
+	updateFilterState?: (newFilterOpts: filterOpts) => void;
+}
+
+// Filtering logic
+
+const handleUpdateFilter = (prevFilter: filterOpts, newFilterOpts: filterOpts) => {
+	console.log(newFilterOpts);
+		return {...prevFilter, ...newFilterOpts};
+}
+
+const checkFilter = (filter: string, product: Product) => {
+	if (filter.length === 0) return true
+	return product.categories.includes(filter);
+}
+
+export const filterContext = createContext<filterContext>({});
 
 const AllProducts = ({products}: {products: ProductArray}) => {
 	
+	const [filterState, updateFilterState] = useReducer(handleUpdateFilter, initialFilter);
+
+	const filteredProducts = products.filter(product => 
+		checkFilter(filterState.productType!, product) &&
+		checkFilter(filterState.edition!, product) &&
+		checkFilter(filterState.style!, product)
+		);
+	console.log(products)
+
 	return (
 		<>
 			<h1 className={classes.title}>All Products</h1>
-			<ProductFilters />
+			<filterContext.Provider value={{filterState: filterState, updateFilterState: updateFilterState}}>
+				<ProductFilters />
+			</filterContext.Provider>
 			<main className={classes.allProductsWrapper}>
-				{products && products.map(product => 
-					<ProductPreview product={product} key={product.id} />
-					)}
+				{filteredProducts.length > 0 ? 
+					filteredProducts.map(product => 
+						<ProductPreview product={product} key={product.id} />
+					)
+				:
+					<h2>No matching products found</h2>
+					}
 			</main>
 		</>
 	)
 }
 export const getStaticProps: GetStaticProps = async (context) => {
-	const res = await fetch("https://res.cloudinary.com/dhqlxce9z/raw/upload/v1648783422/perseus/products_wbtxfs.json", {
+	const res = await fetch("https://res.cloudinary.com/dhqlxce9z/raw/upload/v1648844176/perseus/products_yhyy0v.json", {
 		method: "GET"
 	})
 	.then(res => res.json());
