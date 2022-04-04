36877d2c29ca8e1da265020b0070560c691c08a6
azat valiev <azvaliev@gmail.com>
2022-04-03 17:34:10 -0700

    Basic reccomended products section

--- pages/product/[pid].tsx
+++ pages/product/[pid].tsx
@@ -1,12 +1,22 @@
+import { match } from "assert";
 import { GetStaticProps } from "next";
 import ProductView from "../../components/product/ProductView";
+import ProductPreview from "../../components/products/ProductPreview";
 import { Product, ProductArray } from "../../components/products/ProductTypes";
-import classes from "../../styles/modules/Product.module.scss";
+import styles from "../../styles/modules/Product.module.scss";
 
-const ProductDisplay = ({product}: {product: Product}) => {
+const ProductDisplay = ({product, reccomendedProducts}: {product: Product, reccomendedProducts: Product[]}) => {
 	return (
-		<div className={classes["wrapper-page-product"]}>
+		<div className={styles["wrapper-page-product"]}>
 			<ProductView product={product} />
+			<section className={styles["reccomended-wrapper"]}>
+				<h2>Reccomended Products</h2>
+				<div className={`reccomended ${styles["reccomended"]}`}>
+					{reccomendedProducts.map((rec) => 
+						<ProductPreview product={rec} key={product.id} />
+						)}
+				</div>
+			</section>
 		</div>
 	)
 }
@@ -14,18 +24,40 @@ const ProductDisplay = ({product}: {product: Product}) => {
 export const getStaticProps: GetStaticProps = async(context) => {
 	
 	const { pid } = context.params!; 
-	const res = await fetch("https://res.cloudinary.com/dhqlxce9z/raw/upload/v1648857188/perseus/products_bvqqdk.json", {
+	const [res, reccomendedProducts] = await fetch("https://res.cloudinary.com/dhqlxce9z/raw/upload/v1648857188/perseus/products_bvqqdk.json", {
 		method: "GET"
 	})
 	.then(res => res.json())
-	.then((res: ProductArray) => 
-		res.find(res => res.id === Number(pid))
-	)
+	.then((res: ProductArray) => {
+		const product = res.find(res => res.id === Number(pid));
+		console.log("product is", product);
+		const reccomendedProducts: Product[] = [];
+		reccomendedProducts.push(res[res.length - 1]);
+		res.forEach((item: Product) => {
+			if (reccomendedProducts.length < 3 && 
+				item.categories.includes(product!.categories[0]) &&
+				item.id !== product!.id) {
+				reccomendedProducts.push(item);
+			}
+		});
+		if (reccomendedProducts.length < 3) {
+			const matching = res.filter(item => item.categories.includes(product!.categories[1]));
+			matching.forEach(match => {
+					if (!reccomendedProducts.includes(match) && 
+					reccomendedProducts.length < 3 &&
+					product!.id !== match.id) { 
 
+						reccomendedProducts.push(match);
+					}
+			});
+		}
+		return [product, reccomendedProducts]
+	})
 
 	return {
 		props: {
-			product: res
+			product: res,
+			reccomendedProducts: reccomendedProducts
 		},
 		revalidate: 10
 	}
@@ -39,9 +71,7 @@ export const getStaticPaths = async() => {
 	.then(res => res.json())	
 	.then((res: ProductArray) => {
 		return res.map(res => ( { params: { pid: res.id.toString()} }))
-	}
-)
-
+	})
 	return {
 	  paths: res,
 	  fallback: true
