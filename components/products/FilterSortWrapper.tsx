9f8ce4f926f93ff59759a401460b793926159049
azat valiev <azvaliev@gmail.com>
2022-04-06 11:50:09 -0700

    Fully functional filter and sort page for mobile.

--- components/products/FilterSortWrapper.tsx
+++ components/products/FilterSortWrapper.tsx
@@ -1,41 +1,56 @@
-import { createPortal } from "react-dom";
-import usePortalMountCheck from "../../lib/usePortalMountCheck";
-import useMobileCheck from "../../lib/useMobileCheck";
-import ProductFilters from "./ProductFilters";
-import Sorting from "./Sorting";
-import useStateToggle from "../../lib/useStateToggle";
-
+import { createPortal } from 'react-dom';
+import usePortalMountCheck from '../../lib/usePortalMountCheck';
+import useMobileCheck from '../../lib/useMobileCheck';
+import ProductFilters from './ProductFilters';
+import Sorting from './Sorting';
+import useStateToggle from '../../lib/useStateToggle';
 
 const FilterSortWrapper = () => {
 	const isMounted = usePortalMountCheck();
 	const isMobile = useMobileCheck();
 	const [sortPageOpen, toggleSortPageOpen] = useStateToggle(false);
-	
-	return isMounted ? createPortal(
-		<div id="wrapper-filter-sort" className={sortPageOpen && isMobile ? "fullscreen" : ""}>
-			{!isMobile || sortPageOpen ?
-				<>
-					<ProductFilters />
-					<Sorting />
-				</>
-			:
-				<>
-					<h4>Sort / Filter</h4>
-					<svg 
-						focusable="false"
-						aria-hidden="true"
-						viewBox="0 0 24 24"
-						fill="#ebe1b5"
-						onClick={toggleSortPageOpen}
-						>
-						<path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/>
-					</svg>
-				</>
-			}
-		</div>, 
-		document.querySelector("#filterbar")!
-	): null
-	
-}
+
+	return isMounted
+		? createPortal(
+				<div
+					id="wrapper-filter-sort"
+					className={sortPageOpen && isMobile ? 'fullscreen' : ''}
+				>
+					{!isMobile || sortPageOpen ? (
+						<>
+							{isMobile && (
+								<svg
+									focusable="false"
+									aria-label="Close Sort / Filter Menu"
+									viewBox="0 0 24 24"
+									fill="#ebe1b5"
+									onClick={toggleSortPageOpen}
+									className="open"
+								>
+									<path d="m12 8-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14z"></path>
+								</svg>
+							)}
+							<ProductFilters />
+							<Sorting />
+						</>
+					) : (
+						<>
+							<h4>Sort / Filter</h4>
+							<svg
+								focusable="false"
+								aria-label="Open Sort and Filter page"
+								viewBox="0 0 24 24"
+								fill="#ebe1b5"
+								onClick={toggleSortPageOpen}
+							>
+								<path d="M16.59 8.59 12 13.17 7.41 8.59 6 10l6 6 6-6z"></path>
+							</svg>
+						</>
+					)}
+				</div>,
+				document.querySelector('#filterbar')!
+		  )
+		: null;
+};
 
 export default FilterSortWrapper;
